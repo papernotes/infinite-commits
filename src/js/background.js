0d7d8ed60865d150c70d125196b266550378e3fb
Jonathan Perapalanunt <jpapernotes@gmail.com>
2017-03-26 01:37:50 -0700

    Set icon to enable or disable if the user is on a commits page

--- src/js/background.js
+++ src/js/background.js
@@ -4,9 +4,9 @@
  */
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (/https:\/\/github\.com\/[^\/]+\/[^\/]+\/commits/g.exec(tab.url) != null) {
-    chrome.browserAction.setIcon({path: "src/icons/active128.png", tabId:tab.id});
+    chrome.browserAction.enable(tab.id);
   }
   else {
-    chrome.browserAction.setIcon({path: "src/icons/inactive128.png", tabId:tab.id});
+    chrome.browserAction.disable(tab.id);
   }
 });
